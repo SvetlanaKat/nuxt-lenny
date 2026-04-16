fa5b0a0c85c27ff99b825657179b63e86ed77dc2
SvetlanaKat <svetlanakatrovskaa790@gmail.com>
2026-04-16 15:35:50 +0300

    чек-бокс на детальной

--- app/components/ReserveChecks.vue
+++ app/components/ReserveChecks.vue
@@ -1,5 +1,105 @@
 <template>
- <div class="reserve__checks">
+
+<div class="reserve__checks">
+    <h4 class="reserve__section-title">
+      Забронировать столик
+    </h4>
+
+    <div class="reserve__book">
+    <FieldCheck
+      name="agreement"
+      :rules="{ required: true }"
+      :submitCount="submitCount"
+      :label="'1 стол'"
+      smallLabel
+      class="reserve__check check check--red"
+    />
+    <FieldCheck
+      name="agreement"
+      :rules="{ required: true }"
+      :submitCount="submitCount"
+      :label="'2 стол'"
+      smallLabel
+      class="reserve__check check"
+    />
+    <FieldCheck
+      name="agreement"
+      :rules="{ required: true }"
+      :submitCount="submitCount"
+      :label="'3 стол'"
+      smallLabel
+      class="reserve__check check"
+    />
+    <FieldCheck
+      name="agreement"
+      :rules="{ required: true }"
+      :submitCount="submitCount"
+      :label="'4 стол'"
+      smallLabel
+      class="reserve__check check check--red"
+    />
+    <FieldCheck
+      name="agreement"
+      :rules="{ required: true }"
+      :submitCount="submitCount"
+      :label="'5 стол'"
+      smallLabel
+      class="reserve__check check check--disabled"
+    />
+    <FieldCheck
+      name="agreement"
+      :rules="{ required: true }"
+      :submitCount="submitCount"
+      :label="'6 стол'"
+      smallLabel
+      class="reserve__check check"
+    />
+    <FieldCheck
+      name="agreement"
+      :rules="{ required: true }"
+      :submitCount="submitCount"
+      :label="'7 стол'"
+      smallLabel
+      class="reserve__check check check--red"
+    />
+    <FieldCheck
+      name="agreement"
+      :rules="{ required: true }"
+      :submitCount="submitCount"
+      :label="'8 стол'"
+      smallLabel
+      class="reserve__check check check--red"
+    />
+    <FieldCheck
+      name="agreement"
+      :rules="{ required: true }"
+      :submitCount="submitCount"
+      :label="'9 стол'"
+      smallLabel
+      class="reserve__check check check--red"
+    />
+    <FieldCheck
+      name="agreement"
+      :rules="{ required: true }"
+      :submitCount="submitCount"
+      :label="'10 стол'"
+      smallLabel
+      class="reserve__check check"
+    />
+    <FieldCheck
+      name="agreement"
+      :rules="{ required: true }"
+      :submitCount="submitCount"
+      :label="'11 стол'"
+      smallLabel
+      class="reserve__check check"
+    />
+    </div>
+  </div>
+
+  
+
+ <!-- <div class="reserve__checks">
     <h4 class="reserve__section-title">
       Забронировать столик
     </h4>
@@ -118,10 +218,115 @@
    
     
     </div>
-  </div>
+  </div> -->
 </template>
 
-<script setup></script>
+<script setup>
+// const reserve = $("#js-reserve");
+// if (reserve.length) {
+//   const reservePrice = JSON.parse(reserve.attr("data-price"));
+//   const reserveTypes = Object.keys(reservePrice);
+//   const reserveTicketData = {};
+
+//   const reserveCheck = reserve.find(".js-reserveCheck");
+//   const reserveScene = reserve.find("#js-reserveScene");
+
+//   const reservePriceList = reserve.find("#js-reservePriceList");
+//   const reservePriceCaption = reservePriceList.attr("data-caption");
+
+//   const reserveTicketsList = reserve.find("#js-reserveTicketsList");
+//   const reserveTicketsCaption = reserveTicketsList.attr("data-caption");
+//   const reserveTicketsCurrency = reserveTicketsList.attr("data-caption");
+
+//   const reserveTotal = reserve.find("#js-reserveTotal");
+//   const reserveSubmit = reserve.find("#js-reserveSubmit");
+
+//   const sumReserve = function () {
+//     let total = 0;
+
+//     reserveTypes.forEach(function (type) {
+//       const qty = reserveTicketData[type].qty;
+//       const sum = reserveTicketData[type].qty * reservePrice[type];
+//       reserveTicketData[type].qtyE1.html(qty);
+//       reserveTicketData[type].sumE1.html(numberWithSpaces(sum));
+//       total += sum;
+//     });
+
+//     reserveTotal.html(numberWithSpaces(total));
+
+//     if (total > 0) {
+//       reserveSubmit.removeClass("btn--disabled");
+//     } else {
+//       reserveSubmit.addClass("btn--disabled");
+//     }
+//   };
+
+//     reserveTypes.forEach(function (type) {
+//       reservePriceList.append(
+//          `<div class="reserve__price-item reserve__price-item--${type}" "data-caption="${reservePriceCaption}>
+//         ${reservePrice[type]} + 
+//         </div>`
+//       );
+
+//       const reserveTicketsE1 = $(`<div class="reserve__tickets">`);
+//       const reserveQtyE1 = $(
+//         `<div class="reserve__qty" data-caption="${reserveTicketsCaption}" data-price="${reservePrice[type]}">`
+//       );
+//       const reserveSumE1 = $(
+//         `<div class="reserve__sum" data-currency="${reserveTicketsCurrency}">`
+//       );
+
+//       reserveTicketsE1.append(reserveQtyE1);
+//       reserveTicketsE1.append(reserveSumE1);
+
+//       reserveTicketsList.append(reserveTicketsE1);
+
+//       reserveTicketData[type] = {};
+//       reserveTicketData[type].qty = 0;
+//       reserveTicketData[type].qtyE1 = reserveQtyE1;
+//       reserveTicketData[type].sumE1 = reserveSumE1;
+//     });
+
+//     $.each(reserveCheck, function (i) {
+//       const check = this;
+//       const name = $(check).attr("name");
+//       if (!name) return;
+//       const sceneTable = reserveScene.find("[data-check=" + name + "]");
+//       if ($(this).attr("disabled")) {
+//         sceneTable.addClass("scene__table--disabled");
+//         return;
+//       }
+
+//       const type = $(check).attr("data-type") || "black";
+
+//       if (check.checked) {
+//         reserveTicketsData[type].qty += 1;
+//         sceneTable.addClass("scene__table--active");
+//       }
+
+//       $(check).on("change", function () {
+//         if (this.checked) {
+//           reserveTicketsData[type].qty += 1;
+//           sceneTable.addClass("scene__table--active");
+//         } else {
+//           if (reserveTicketsData[type].qty > 0) 
+//             reserveTicketsData[type].qty -= 1;
+//           sceneTable.removeClass("scene__table--active");
+//         }
+
+//         sumReserve();
+//       });
+
+//       sceneTable.on("click", function () {
+//         check.checked = !check.checked;
+//         $(check).trigger("change");
+//       });
+//     });
+
+//     sumReserve();
+
+//   }
+</script>
 
 <style lang="less">
 .reserve {
@@ -187,6 +392,7 @@
   &__check {
     margin-top: 0;
     margin-bottom: 10px;
+    padding-left: 0;
   }
 
   &__reserve>div {
